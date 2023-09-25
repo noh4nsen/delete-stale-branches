795259c96745cfce9714abe85df1353440b06d85
Raphael Rodrigues <noh4nsen@gmail.com>
2023-09-25 19:21:54 -0300

    feat: better logging

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ github_instance = Github(auth=auth)
 repo = github_instance.get_repo(repo)
 branches = repo.get_branches()
 
+qty_deleted_branches = 0
+
 for branch in branches:
     if 'stale/' in branch.name:
+        qty_deleted_branches += 1
         print("-- Deleting branch", branch.name, "--")
         ref = repo.get_git_ref(f"heads/{branch.name}")
         ref.delete()
+print(f" \n-- {qty_deleted_branches} deleted branches --")
